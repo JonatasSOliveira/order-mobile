b66858ae7a72ad391725608d350c9a68404b8961
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2022-07-05 19:25:11 -0300

    Fixing Product Model

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,53 +1,52 @@
-
 import BaseModel, { ColumnsMappingType } from "../services/BaseModel";
 
-export default class Product extends BaseModal {
-    private name: string;
-    private description: string;
-    private price: number;
+export default class Product extends BaseModel {
+  private name: string = "";
+  private description: string = "";
+  private price: number = 0;
 
-    protected static get tableName(): string {
-        return "product";
-    }
+  protected static get tableName(): string {
+    return "product";
+  }
 
-    protected static get columnsMapping(): ColumnsMappingType {
-        return {
-            name: {
-                type: "TEXT",
-                required: true,
-            },
-            description: {
-                type: "TEXT",
-                required: false
-            },
-            price: {
-                type: "NUMERIC",
-                required: true
-            }
-        };
-    }
+  protected static get columnsMapping(): ColumnsMappingType {
+    return {
+      name: {
+        type: "TEXT",
+        required: true,
+      },
+      description: {
+        type: "TEXT",
+        required: false,
+      },
+      price: {
+        type: "NUMERIC",
+        required: true,
+      },
+    };
+  }
 
-    public getName() {
-        return this.name;
-    }
+  public getName() {
+    return this.name;
+  }
 
-    public setName(name: string) {
-        this.name = name;
-    }
+  public setName(name: string) {
+    this.name = name;
+  }
 
-    public getDescription() {
-        return this.description;
-    }
+  public getDescription() {
+    return this.description;
+  }
 
-    public setDescription(description: string) {
-        this.description = description;
-    }
+  public setDescription(description: string) {
+    this.description = description;
+  }
 
-    public getPrice() {
-        return this.price;
-    }
+  public getPrice() {
+    return this.price;
+  }
 
-    public setPrice(price: number) {
-        this.price = price;
-    }
+  public setPrice(price: number) {
+    this.price = price;
+  }
 }
