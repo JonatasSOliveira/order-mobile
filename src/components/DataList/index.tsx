9d37eb4accb52e8d1bef973ebb800cc0c4e90d0e
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2022-07-05 19:24:46 -0300

    Fixing DataList Component

--- src/components/DataList/index.tsx
+++ src/components/DataList/index.tsx
@@ -11,83 +11,89 @@ import { Container, ListContainer, Row } from "./styles";
 import RootStackParamList from "../../components/StackNavigator/RootStackParamList";
 
 import lightTheme from "../../styles/themes/light";
-
+import BaseModel from "../../services/BaseModel";
 
 type drawerNavigatorProp = StackNavigationProp<
-    RootStackParamList,
-    "DrawerNavigator"
+  RootStackParamList,
+  "DrawerNavigator"
 >;
 
-interface CommonProps {
-    getRowText(item: T): string
-    onPressEdit(item: T): void
+interface CommonProps<T extends BaseModel> {
+  getRowText(item: T): string;
+  onPressEdit(item: T): void;
 }
 
-interface DataRowProps extends CommonProps {
-    item: T
+interface DataRowProps<T extends BaseModel> extends CommonProps<T> {
+  item: T;
 }
 
-interface DataListProps extends CommonProps {
-    loadData(): Promisse<T>
-    buttonLabel: string
-    onPressNew(): void
+interface DataListProps<T extends BaseModel> extends CommonProps<T> {
+  loadData(): Promise<Array<T>>;
+  buttonLabel: string;
+  onPressNew(): void;
 }
 
-function DataRow({
-    getRowText,
-    onPressEdit,
-    item
-}: DataRowProps) {
-    return (
-        <Row>
-            <Text>{getRowText(item)}</Text>
-            <IconButton
-                icon="square-edit-outline"
-                onPress={() => onPressEdit(item)}
+function DataRow<T extends BaseModel>({
+  getRowText,
+  onPressEdit,
+  item,
+}: DataRowProps<T>) {
+  return (
+    <Row>
+      <Text>{getRowText(item)}</Text>
+      <IconButton
+        icon="square-edit-outline"
+        onPress={() => onPressEdit(item)}
+      />
+    </Row>
+  );
+}
+
+export default function DataList<T extends BaseModel>({
+  loadData,
+  buttonLabel,
+  onPressNew,
+  getRowText,
+  onPressEdit,
+}: DataListProps<T>) {
+  const [data, setData] = useState<Array<T>>([]);
+
+  const navigation = useNavigation<drawerNavigatorProp>();
+
+  async function loadDataHandler() {
+    const data = await loadData();
+    setData(data);
+  }
+
+  useEffect(() => {
+    loadDataHandler();
+  }, []);
+
+  return (
+    <Container>
+      <ListContainer>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <DataRow
+              item={item}
+              getRowText={getRowText}
+              onPressEdit={onPressEdit}
             />
-        </Row>
-    );
-}
-
-export default function DataList({ loadData, buttonLabel, onPressNew, getRowText, onPressEdit }: DataListProps) {
-    const [data, setData] = useState([]);
-
-    const navigation = useNavigation<drawerNavigatorProp>();
-
-    async function loadDataHandler() {
-        const data = await loadData();
-        setData(data);
-    }
-
-    useEffect(() => {
-        loadDataHandler();
-    }, [])
-
-    return (
-        <Container>
-            <ListContainer>
-                <FlatList
-                    data={data}
-                    renderItem={({ item }) => (
-                        <DataRow
-                            item={item}
-                            getRowText={getRowText}
-                            onPressEdit={onPressEdit}
-                        />
-                    )}
-                    keyExtractor={(item) => item.getId().toString()}
-                />
-            </ListContainer>
-            <View>
-                <Button
-                    mode="contained"
-                    color={lightTheme.PRIMARY}
-                    icon="plus-circle"
-                    onPress={onPressNew}
-                >
-                    {buttonLabel}
-                </Button>
-            </View>
-        </Container>
-    );
+          )}
+          keyExtractor={(item) => item.getId().toString()}
+        />
+      </ListContainer>
+      <View>
+        <Button
+          mode="contained"
+          color={lightTheme.PRIMARY}
+          icon="plus-circle"
+          onPress={onPressNew}
+        >
+          {buttonLabel}
+        </Button>
+      </View>
+    </Container>
+  );
 }
