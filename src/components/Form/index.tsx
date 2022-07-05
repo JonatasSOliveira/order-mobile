6e5519198b1d107b1a126448b1f885e11259b1f2
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2022-07-05 19:25:52 -0300

    Creating Form Component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { Container, Footer, FooterButton, InputsContainer } from "./styles";
+import lightTheme from "../../styles/themes/light";
+
+interface FormProps {
+  children: JSX.Element;
+  onCancel: () => void;
+  onSave: () => void;
+}
+
+export default function Form({ children, onCancel, onSave }: FormProps) {
+  return (
+    <Container>
+      <InputsContainer>{children}</InputsContainer>
+      <Footer>
+        <FooterButton
+          mode="contained"
+          color={lightTheme.CANCEL}
+          icon="close-circle"
+          onPress={onCancel}
+        >
+          Cancelar
+        </FooterButton>
+        <FooterButton
+          mode="contained"
+          color={lightTheme.SUCCESS}
+          icon="check-circle"
+          onPress={onSave}
+        >
+          Salvar
+        </FooterButton>
+      </Footer>
+    </Container>
+  );
+}
